0c2a40ad5135b5263782e88af24b281a5f5e63a0
sandhyajig <sandhyagogineni315@gmail.com>
2019-05-30 11:55:31 +0530

    jj

---
+++ src/test/java/r/MySteps.java
@@ -0,0 +1,8 @@
+package r;
+
+import br.gov.frameworkdemoiselle.behave.parser.Step;
+import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
+
+public class MySteps extends CommonSteps implements Step {
+
+}
---
+++ src/test/java/r/MyTest.java
@@ -0,0 +1,20 @@
+package r;
+
+import org.junit.Test;
+
+import br.gov.frameworkdemoiselle.behave.controller.BehaveContext;
+
+public class MyTest {
+
+	private BehaveContext eng = BehaveContext.getInstance();
+
+	public MyTest() {
+		eng.addSteps(new MySteps());
+	}
+
+	@Test
+	public void testAllStories() throws Throwable {
+		eng.run("/stories");
+	}
+
+}
